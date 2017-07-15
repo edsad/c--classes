4bf7591382a39bf12d6c6d8f24aad375bb458572
Ed Sadler <ed_lorie@comcast.net>
2017-07-15 17:57:20 -0500

    complete?

--- Program.cs
+++ Program.cs
@@ -8,15 +8,83 @@ namespace classes
         static void Main(string[] args)
         {
 
-                        public class Company
-            {
-                /*
-                    Some readonly properties
-                */
-                public string Name { get; }
-                public DateTime CreatedOn { get; }
+                        // public class Company
+            
+
+                // public string Name { get; }
+                // public DateTime CreatedOn { get; }
 
                 // Create a property for holding a list of current employees
+
+                Company company = new Company ("LilCakes", DateTime.Now);
+
+                // I know it said to add 3 but I wanted four!
+
+                Employee employee1 = new Employee ("Lorie", "Baker", "June 14, 2010");
+                Employee employee2 = new Employee ("Brandie", "Designer", "September 22, 2011");
+                Employee employee3 = new Employee ("Bridgette", "Cashier", "Febuary 5, 2011");
+                Employee employee4 = new Employee ("Liz", "Everything", "October 22, 2010");
+
+                company.addEmployee(employee1);
+                company.addEmployee(employee2);
+                company.addEmployee(employee3);
+                company.addEmployee(employee4);
+
+                company.ListEmployees();
+        }
+            
+        }
+                public class Company
+                {
+
+                 /*
+                    Some read only properties
+                */
+
+                    public string Name { get; }
+                    public DateTime CreatedOn { get; }
+
+                    public List<Employee> listOfEmployees = new List <Employee>();
+
+                    public void addEmployee (Employee employee)
+                    {
+                        listOfEmployees.Add(employee);
+                    }
+
+                    public void removeEmployee (Employee employee)
+                    {
+                        listOfEmployees.Remove(employee);
+                    }
+
+                    public Company (string name, DateTime createdOn)
+                    {
+                        Name = name;
+                        CreatedOn = createdOn;
+                    }
+
+                    public void ListEmployees()
+                        {
+                            foreach (Employee empl in listOfEmployees)
+                            {
+                                Console.WriteLine ($"{empl.Name}, {empl.JobTitle}, {empl.StartDate}");
+                            }
+                        }
+                }
+
+                public class Employee
+                {
+                    public string Name;
+                    public string JobTitle;
+                    public string StartDate;
+
+                    public Employee (string name, string jobTitle, string startDate)
+                    {
+                        Name = name;
+                        JobTitle = jobTitle;
+                        StartDate = startDate;
+                    }
+                }
+    }
 
                 // Create a method that allows external code to add an employee
 
@@ -29,8 +97,5 @@ namespace classes
 
                     The constructor will set the value of the public properties
                 */
-            }
-                Console.WriteLine("Hello World!");
-        }
-    }
-}
+ 
+
